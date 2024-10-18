a91f0ba323d9faa783de7804f9d914c952049270
Cleilton Santos <cleilton14s@gmail.com>
2024-10-18 16:56:43 -0300

    Segundo commit

---
+++ app/src/main/java/com/example/chargingcontrol/Motoristas.java
@@ -0,0 +1,5 @@
+package com.example.chargingcontrol;
+
+public class Motoristas {
+    String[] Betoneiros = {"Cleilton","Adailton","Thiago","Tuila","Ubiratan","Edson","Carlos Andre","Rodrigo","Fernando","Jardson","Anderson","Robson","Roberto","Edney","Emerson","Acerola","Antonio","Jorge","Franklin","Marcelo","Ismael","Everton","Vitor","Gilvan","Joao Bala",};
+}
